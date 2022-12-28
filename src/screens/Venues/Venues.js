1778263d7eee6698fdf749c2bd9c0c0102fbe2d3
Sonia <99384358+Sonia-ttn@users.noreply.github.com>
2022-12-28 17:06:54 +0530

    Update Venues.js

--- src/screens/Venues/Venues.js
+++ src/screens/Venues/Venues.js
@@ -27,35 +27,4 @@ function Venues() {
 
 export default Venues
 
-/*import React from 'react'
-import "./Venues.css";
-import img from "./logo.png";
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
 
-function Venues() {
-  return (
-    <div className='container'>
-        <div className='test'>
-        <div className="imagebg">
-            <img src={img} className="image"></img>  
-        </div>
-        <h2>Venues with Most Users</h2>
-        <SearchIcon>
-        <InputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-        </SearchIcon>
-
-        
-        </div>
-        <div>
-
-        </div>
-      
-    </div>
-  )
-}
-
-export default Venues*/
