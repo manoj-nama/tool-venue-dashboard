6fad63dc943ec7a04de6aa6a73916be0044aad82
Swastik Pal <swastik.pal@tabcorp.com.au>
2023-01-04 10:19:31 +0530

    config changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import history from './utils/history';
-import Home from './screens/Home';
-import Venues from './screens/Venues/Venues';
-import './index.scss';
-
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import history from "./utils/history";
+import Home from "./screens/Home";
+import Venues from "./screens/Venues/Venues";
+import "./index.scss";
+import ErrorBoundary from "./utils/errorBoundary";
 
 const AppWrapper = () => (
   <BrowserRouter history={history}>
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <ErrorBoundary>
             <Home />
+          </ErrorBoundary>
         }
-        />
-         <Route
-          exact
-          path="/venue"
-          element={
-            <Venues  />
+      />
+      <Route
+        exact
+        path="/venue"
+        element={
+          <ErrorBoundary>
+            <Venues />
+          </ErrorBoundary>
         }
-        />
-      </Routes>
+      />
+    </Routes>
   </BrowserRouter>
 );
 
